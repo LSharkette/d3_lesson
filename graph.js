57e7c670f24b9ac0c8bd031af1ab0c52c78f753d
Mason F. Matthews <mason.f.matthews@gmail.com>
2015-06-29 21:48:48 -0400

    Finish lecture part 3

--- graph.js
+++ graph.js
@@ -1,46 +1,85 @@
-svg = d3.select("svg");
-g = svg.append("g");
+var svg = d3.select("svg");
+
+var g = svg.append("g");
 g.attr("transform", "translate(100,50)");
 
-x = d3.scale.linear()
+var gx = g.append("g")
+gx.attr("transform", "translate(0,400)");
+
+var x = d3.scale.linear()
     .domain([2000, 2012])
     .range([0, 800]);
-y = d3.scale.linear()
+var y = d3.scale.linear()
     .domain([0, 100])
     .range([400, 0]);
 
-x_axis = d3.svg.axis().scale(x).orient("bottom").ticks(5).tickFormat(d3.format("d"));
-y_axis = d3.svg.axis().scale(y).orient("left").ticks(4);
-
+var y_axis = d3.svg.axis().scale(y).orient("left").ticks(4);
 g.call(y_axis);
 
-gx = g.append("g")
+var x_axis = d3.svg.axis().scale(x).orient("bottom").ticks(5).tickFormat(d3.format("d"));
 gx.call(x_axis);
-gx.attr("transform", "translate(0,400)");
 
-d3.csv("old_discoveries.csv", function(data) {
+d3.csv("data.csv", function(data) {
   g.selectAll("circle")
       .data(data)
     .enter().append("circle")
       .attr("cx", function(d) {return x(d["year"]);} )
-      .attr("cy", function(d) {return y(d["important_discoveries"]);} )
-      .attr("r", 6);
+      .attr("cy", function(d) {return y(d["accidental_deaths_in_thousands"]);} )
+      .attr("r", 10);
 });
 
-function updateDiscoveries() {
-  d3.csv("new_discoveries.csv", function(data) {
+
+function changeY() {
+  var field = event.target.value;
+  d3.csv("data.csv", function(data) {
     join = g.selectAll("circle")
-        .data(data);
-
-    join.attr("cx", function(d) {return x(d["year"]);} )
-        .attr("cy", function(d) {return y(d["important_discoveries"]);} );
-
-    join.enter().append("circle")
-        .attr("cx", function(d) {return x(d["year"]);} )
-        .attr("cy", function(d) {return y(d["important_discoveries"]);} );
-
-    join.exit().remove();
+        .data(data)
+        .transition().duration(1000)
+        .attr("cy", function(d) {return y(d[field]);} );
   });
 }
+document.getElementById("change-y").onchange = changeY;
 
-document.getElementById("update_button").onclick = updateDiscoveries;
+
+function changeR() {
+  var field = event.target.value;
+  var radiusFunction = function(d) {return d[field]/4;}
+  if (field==="none") {
+    radiusFunction = function(d) {return 10;};
+  }
+  d3.csv("data.csv", function(data) {
+    join = g.selectAll("circle")
+        .data(data)
+        .transition().duration(1000)
+        .attr("r", radiusFunction );
+  });
+}
+document.getElementById("change-r").onchange = changeR;
+
+
+var fillScale = d3.scale.linear()
+    .domain([0, 100])
+    .range(["hsl(0, 100, 80)", "hsl(120, 100, 80)"])
+    .interpolate(d3.interpolateHcl);
+var strokeScale = d3.scale.linear()
+    .domain([0, 100])
+    .range(["hsl(0, 100, 35)", "hsl(120, 100, 35)"])
+    .interpolate(d3.interpolateHcl);
+
+function changeColor() {
+  var field = event.target.value;
+  var fillFunction = function(d) {return fillScale(d[field]);}
+  var strokeFunction = function(d) {return strokeScale(d[field]);}
+  if (field==="none") {
+    fillFunction = function(d) {return "lightblue";};
+    strokeFunction = function(d) {return "darkblue";};
+  }
+  d3.csv("data.csv", function(data) {
+    join = g.selectAll("circle")
+        .data(data)
+        .transition().duration(1000)
+        .style("fill", fillFunction )
+        .style("stroke", strokeFunction );
+  });
+}
+document.getElementById("change-color").onchange = changeColor;
